7ae34710825dcfaf2688b396d71fe975b80da76a
yasmine <yasmineabass28100@gmail.com>
2025-06-14 17:46:24 +0200

    WIP: Save local changes before pulling main

---
+++ AdviLaw.Application/Job/Dtos/CreateJobDto.cs
@@ -0,0 +1,18 @@
+﻿using AdviLaw.Domain.Entites.JobSection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdviLaw.Application.Job.Dtos
+{
+  public class CreateJobDto
+    {
+        public string Header { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int Budget { get; set; }
+        public string JobFieldName { get; set; } = string.Empty;
+        public bool IsAnonymous { get; set; }
+    }
+}
