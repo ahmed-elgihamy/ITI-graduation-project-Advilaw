76a2dbdf67acb84c621ef7df80100b8073f5ab68
yasmine <yasmineabass28100@gmail.com>
2025-06-26 23:09:54 +0200

    ReviewsOfeachLAW

---
+++ AdviLaw.Infrastructure/Repositories/ReviewRepository.cs
@@ -0,0 +1,35 @@
+﻿using AdviLaw.Domain.Entites.SessionUtilities.ReviewSection;
+using AdviLaw.Domain.Repositories;
+using AdviLaw.Infrastructure.GenericRepo;
+using AdviLaw.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdviLaw.Infrastructure.Repositories
+{
+    public class ReviewRepository : GenericRepository<Review>, IReviewRepository
+    {
+
+        private readonly AdviLawDBContext _dbContext;
+        public ReviewRepository(AdviLawDBContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<Review>> GetReviewsByLawyerId(int lawyerId)
+        {
+            return await _dbContext.Reviews
+                .Include(r => r.Reviewer)
+                .Include(r => r.Reviewee)
+                .Where(r => r.Reviewee != null && r.Reviewee.LawyerId == lawyerId)
+                .OrderByDescending(r => r.CreatedAt)
+                .ToListAsync();
+        }
+
+
+    }
+}
